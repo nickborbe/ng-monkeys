a472fdbeafd69de4fe53a65ce72fa79e8146d722
Nicholas Borbe <nickborbe@gmail.com>
2017-09-23 11:08:20 -0400

    whichWorld

--- src/app/truth.service.ts
+++ src/app/truth.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 import 'rxjs/add/operator/toPromise';
+
 
 @Injectable()
 export class TruthService {
 BASE_URL = "http://localhost:3000"
+
+whichWorld:BehaviorSubject<string> = new BehaviorSubject("new");
+
 
   constructor(private myHttp: Http) { }
 
@@ -12,6 +17,18 @@ BASE_URL = "http://localhost:3000"
     return this.myHttp.get(`${this.BASE_URL}/api_monkeys`)
     .toPromise()
     .then(apiResponse => apiResponse.json())
+  }
+
+  changeWorld()  {
+    var world;
+
+    this.whichWorld.subscribe((result)=>{ world = result});
+
+    if(world === "new"){
+      this.whichWorld.next("old");
+    }else if(world ==="old"){
+      this.whichWorld.next("new");
+    }
   }
 
 }
