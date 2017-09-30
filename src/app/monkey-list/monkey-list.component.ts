506f1aa396a5929cefc2d1900bc924ba40687086
Nicholas Borbe <nickborbe@gmail.com>
2017-09-30 00:55:59 -0400

    changing family to sub_family

--- src/app/monkey-list/monkey-list.component.ts
+++ src/app/monkey-list/monkey-list.component.ts
@@ -51,11 +51,11 @@ export class MonkeyListComponent implements OnInit {
   this.truthService.getMonkeys()
     .then((list) => {
     this.monkeys = list;
-    this.callitrichidae = list.filter((monkey)=>{ return monkey.family === "Callitrichidae"});
-    this.cebidae = list.filter((monkey)=>{ return monkey.family === "Cebidae"});
-    this.atelidae = list.filter((monkey)=>{ return monkey.family === "Atelidae"});
-    this.aotidae = list.filter((monkey)=>{ return monkey.family === "Aotidae"});
-    this.pitheciidae = list.filter((monkey)=>{ return monkey.family === "Pitheciidae"});
+    this.callitrichidae = list.filter((monkey)=>{ return monkey.sub_family === "Callitrichidae"});
+    this.cebidae = list.filter((monkey)=>{ return monkey.sub_family === "Cebidae"});
+    this.atelidae = list.filter((monkey)=>{ return monkey.sub_family === "Atelidae"});
+    this.aotidae = list.filter((monkey)=>{ return monkey.sub_family === "Aotidae"});
+    this.pitheciidae = list.filter((monkey)=>{ return monkey.sub_family === "Pitheciidae"});
   })
   .catch((err) => {
     this.errorMessage = 'There was an error. Try again later.';
