17e0828bfe0ecc3e5500204d913b03feb7bcf2d4
Nicholas Borbe <nickborbe@gmail.com>
2017-10-03 18:58:11 -0400

    removed clutter

--- src/app/monkey-details/monkey-details.component.ts
+++ src/app/monkey-details/monkey-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute } from '@angular/router';
+import { TruthService } from '../truth.service';
 @Component({
   selector: 'app-monkey-details',
   templateUrl: './monkey-details.component.html',
