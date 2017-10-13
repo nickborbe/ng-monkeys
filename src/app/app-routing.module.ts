3ce4682ce224f3cd7dc42076076a30c6afdb8ac7
Nicholas Borbe <nickborbe@gmail.com>
2017-10-12 23:51:45 -0400

    addMOnkey component wired to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { MonkeyListComponent } from './monkey-list/monkey-list.component';
 import { MonkeyComponent } from './monkey/monkey.component';
 import { MonkeyDetailsComponent } from './monkey-details/monkey-details.component';
+import { AddMonkeyComponent } from './add-monkey/add-monkey.component';
 
 
 const routes: Routes = [
@@ -15,6 +16,10 @@ const routes: Routes = [
   {
     path: 'monkey/:id',
     component: MonkeyDetailsComponent
+  },
+  {
+    path: 'monkey/addnew',
+    component: AddMonkeyComponent
   },
 
 
