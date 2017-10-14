e4f4048c136fbdc9ac74ff5dbb8aec808dbc0ee2
Nicholas Borbe <nickborbe@gmail.com>
2017-10-13 21:17:04 -0400

    steal code from old project

--- src/app/add-monkey/add-monkey.component.ts
+++ src/app/add-monkey/add-monkey.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddMonkeyComponent implements OnInit {
 
+  newMonkey: Object;
+brand: String;
+name: String;
+specs: Array<String>;
+image: String;
+
   constructor() { }
 
   ngOnInit() {
